3956fb47b6691c3e36b122fdcad0136b26031090
luis_gonzalez <luis.gonzalez@expertla.com>
2018-05-08 16:52:44 -0500

    se soluciona bug de limpiar campos en  creacion de pacientes pacientes

--- js/paciente.js
+++ js/paciente.js
@@ -6,7 +6,7 @@ function crarPaciente() {
     var segundoNombrePaciente = document.getElementById('segundoNombrePaciente').value;
     var primerApellidoPaciente = document.getElementById('primerApellidoPaciente').value;
     var segundoApellidoPaciente = document.getElementById('segundoApellidoPaciente').value;
-    var barrioPaciente = document.getElementById('barrioPaciente').value;  
+    var barrioPaciente = document.getElementById('barrioPaciente').value;
     var fechaPaciente = document.getElementById('fechaPaciente').value;
     var sexoPaciente = document.getElementById('sexoPaciente').value;
     var estadoCivilPaciente = document.getElementById('estadoCivilPaciente').value;
@@ -40,7 +40,7 @@ function crarPaciente() {
         });
     } else {
         refEditarPaciente = null;
-        refPaciente = firebase.database().ref("pacientes/"+identificacionPaciente);
+        refPaciente = firebase.database().ref("pacientes/" + identificacionPaciente);
         refPaciente.set({
             primerNombrePaciente: primerNombrePaciente,
             segundoNombrePaciente: segundoNombrePaciente,
@@ -60,15 +60,15 @@ function crarPaciente() {
             celularPaciente: celularPaciente,
             user: user.uid
         });
-        var refHistoriasClinicas = firebase.database().ref("historiasClinicas/"+identificacionPaciente);
+        var refHistoriasClinicas = firebase.database().ref("historiasClinicas/" + identificacionPaciente);
         refHistoriasClinicas.set({
-            alcoholicas:false,
+            alcoholicas: false,
             alergias: false,
-            alimentacion:' ',
+            alimentacion: ' ',
             antecedentesFamiliares: ' ',
             antecedentesSociales: ' ',
-            fuma:false,
-            idPaciente:identificacionPaciente,
+            fuma: false,
+            idPaciente: identificacionPaciente,
             idProfecional: user.uid,
             usoDrogas: ' '
         });
@@ -79,38 +79,37 @@ function crarPaciente() {
 function buscarPacientes() {
     var user = firebase.auth().currentUser;
     //alert("entra a buscar pacientes");
-    
+
     var refPaciente;
     var filasTablaPacientes = "";
-    
-        refPaciente = firebase.database().ref().child("pacientes/");
-       
-        var table = $('#tablePacientesJquery').DataTable();
-        
-        filasTablaPacientes = "";
-        var user = firebase.auth().currentUser;
-        refPaciente.once("value", function (snap) {
-            var datos = snap.val();
-            for (var key in datos) {
-                if (datos[key].user == user.uid) {
-                    //alert(key);
-                    var data =[];
-                    var botonEdit = ' <button type="button" key="' + key + '" onclick="editarPaciente(this)" > <i class=\"fa fa-pencil\" aria-hidden=\"true\" ></i> </button>';
-                    var botonVer = ' <button type="button" key="' + key + '" onclick="editarPaciente(this)" > <i class=\"fa fa-search\" aria-hidden=\"true\" ></i> </button>';
-                    //alert(botonEdit);
-                    data.push(  botonEdit);
-                    data.push( datos[key].primerNombrePaciente+' '+datos[key].primerApellidoPaciente);
-                    data.push( datos[key].identificacionPaciente);
-                  
-                        table.row.add(data ).draw();
-                }
 
+    refPaciente = firebase.database().ref().child("pacientes/");
+
+    var table = $('#tablePacientesJquery').DataTable();
+
+    filasTablaPacientes = "";
+    var user = firebase.auth().currentUser;
+    refPaciente.once("value", function (snap) {
+        var datos = snap.val();
+        for (var key in datos) {
+            if (datos[key].user == user.uid) {
+                //alert(key);
+                var data = [];
+                var botonEdit = ' <button type="button" key="' + key + '" onclick="editarPaciente(this)" > <i class=\"fa fa-pencil\" aria-hidden=\"true\" ></i> </button>';
+                var botonVer = ' <button type="button" key="' + key + '" onclick="editarPaciente(this)" > <i class=\"fa fa-search\" aria-hidden=\"true\" ></i> </button>';
+                //alert(botonEdit);
+                data.push(botonEdit);
+                data.push(datos[key].primerNombrePaciente + ' ' + datos[key].primerApellidoPaciente);
+                data.push(datos[key].identificacionPaciente);
+
+                table.row.add(data).draw();
             }
-            
-           
-           // tablaPacientes.innerHTML = filasTablaPacientes;
-        });
-    
+
+        }
+
+        // tablaPacientes.innerHTML = filasTablaPacientes;
+    });
+
 
 }
 
@@ -136,4 +135,24 @@ function editarPaciente(btn) {
         document.getElementById('celularPaciente').value = snap.val().celularPaciente;
         //alert(result);
     });
+}
+
+function limpiarCrearPaciente(btn) {
+    //alert("aqui llega");
+    $("#crearPaciente").modal("show");
+        document.getElementById('primerNombrePaciente').value = '';
+        document.getElementById('segundoNombrePaciente').value = '';
+        document.getElementById('primerApellidoPaciente').value = '';
+        document.getElementById('segundoApellidoPaciente').value = '';
+        document.getElementById('barrioPaciente').value = '';
+        document.getElementById('sexoPaciente').value = '';
+        document.getElementById('estadoCivilPaciente').value = '';
+        document.getElementById('EscolaridadPaciente').value = '';
+        document.getElementById('tipoIdentificacionPaciente').value = '';
+        document.getElementById('identificacionPaciente').value = '';
+        document.getElementById('religionPaciente').value = '';
+        document.getElementById('direccionPaciente').value = '';
+        document.getElementById('emailPaciente').value = '';
+        document.getElementById('fijoPaciente').value = '';
+        document.getElementById('celularPaciente').value = '';
 }
